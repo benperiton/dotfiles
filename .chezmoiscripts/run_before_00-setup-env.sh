b7ffd91cee535ff0d022106cb46c3fddd9309883
Ben Periton <104396+benperiton@users.noreply.github.com>
2026-05-18 14:13:44 +0100

    Update run_before_00-setup-env.sh
    Change initial folders

--- .chezmoiscripts/run_before_00-setup-env.sh
+++ .chezmoiscripts/run_before_00-setup-env.sh
@@ -3,8 +3,12 @@ set -euo pipefail
 
 # Ensure common directories exist
 mkdir -p ~/.config ~/.local/bin
+mkdir -p ~/Coding/{projects,scripts,resources}
+{{- if eq .machine_role "personal" }}
 mkdir -p ~/Coding/projects/{ideas,personal}
-mkdir -p ~/Coding/{scripts,resources}
+{{- else if eq .machine_role "work" }}
+mkdir -p ~/Coding/projects/{ideas,forks}
+{{- end }}
 
 # Set default shell to zsh
 if [ "$SHELL" != "$(command -v zsh)" ] && command -v zsh &>/dev/null; then
